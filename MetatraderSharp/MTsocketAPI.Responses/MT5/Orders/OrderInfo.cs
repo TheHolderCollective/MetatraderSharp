ef031bccef719d59b919788d1b03d34da07cc7b2
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-08-06 14:06:50 -0700

    Action: Added OrderInfo class for MT5. Added GetOrderInfoAsync() to MT5Client class.

---
+++ MetatraderSharp/MTsocketAPI.Responses/MT5/Orders/OrderInfo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+namespace MetatraderSharp.MTsocketAPI.Responses.MT5;
+
+public class OrderInfo
+{
+    [JsonProperty("MSG")]
+    public string Msg { get; set; }
+
+    [JsonProperty("OPENED")]
+    public List<OpenedOrder> OpenedOrder { get; set; }
+
+    [JsonProperty("PENDING")]
+    public List<PendingOrder> PendingOrder { get; set; }
+
+    [JsonProperty("ERROR_ID")]
+    public int ErrorID { get; set; }
+
+    [JsonProperty("ERROR_DESCRIPTION")]
+    public string ErrorDescription { get; set; }
+
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
