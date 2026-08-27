ee261150dd3b6b391466ea2bea43378258285415
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-08-27 14:54:09 -0700

    Action: Added SymbolList extension methods.

---
+++ MetatraderSharp/Extensions/SymbolListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using MetatraderSharp.MTsocketAPI.Responses;
+using System.Text;
+
+namespace MetatraderSharp.Extensions;
+
+public static class SymbolListExtensions
+{
+    public static int SymbolCount(this SymbolList symbolList)
+    {
+        return symbolList.Symbols.Count;
+    }
+
+    public static List<string> GetSymbolNames(this SymbolList symbolList)
+    {
+        List<string> symbolNames = new();
+
+        if (symbolList.SymbolCount() > 0)
+        {
+            foreach (var symbol in symbolList.Symbols)
+            {
+                if (symbol.Name is not null)
+                    symbolNames.Add(symbol.Name);
+            }
+        }
+
+        return symbolNames;
+    }
+
+}
+
