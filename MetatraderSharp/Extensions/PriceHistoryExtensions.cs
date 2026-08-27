fd9b415ed9338b5df1838167af264a83de55afa7
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-08-27 16:31:35 -0700

    Action: Added GetMaxOpenRate and GetMinOpenRate extension methods.

--- MetatraderSharp/Extensions/PriceHistoryExtensions.cs
+++ MetatraderSharp/Extensions/PriceHistoryExtensions.cs
@@ -13,5 +13,32 @@ public static class PriceHistoryExtensions
     {
         return priceHistory.Rates;
     }
+
+    /// <summary>
+    /// Returns first rate found with maximum open value for data set
+    /// </summary>
+    public static Rate? GetMaxOpenRate(this PriceHistory priceHistory)
+    {
+        if (priceHistory.RateCount() > 0)
+        {
+            double maxOpen = priceHistory.Rates.Max(x => x.Open);
+            return (Rate) priceHistory.Rates.Select(x => x).Where(x => x.Open == maxOpen).First();
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Returns first rate found with minimum open value for data set 
+    /// </summary>
+    public static Rate? GetMinOpenRate(this PriceHistory priceHistory)
+    {
+        if (priceHistory.RateCount() > 0)
+        {
+            double minOpen = priceHistory.Rates.Min(x => x.Open);
+            return (Rate)priceHistory.Rates.Select(x => x).Where(x => x.Open == minOpen).First();
+        }
+        return null;
+    }
+
 }
 
