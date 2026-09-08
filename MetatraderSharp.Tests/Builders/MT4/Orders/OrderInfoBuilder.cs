43bdd46064f4d76e2f4538def797ffe780672b64
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-09-08 14:44:24 -0700

    Refactor: Readded CloseTime and PriceClose properties to Trade class. Added corresponding with methods to TradeBuilder class. Added OrderInfoBuilder.

---
+++ MetatraderSharp.Tests/Builders/MT4/Orders/OrderInfoBuilder.cs
@@ -0,0 +1,54 @@
+﻿using MetatraderSharp.MTsocketAPI.Responses.MT4;
+
+namespace MetatraderSharp.Tests.Builders.MT4;
+
+public class OrderInfoBuilder
+{
+    private string _msg;
+    private Trade _trade;
+    private int _errorID;
+    private string _errorDescription;
+
+    public OrderInfoBuilder()
+    {
+        _msg = "ORDER_INFO";
+        _trade = new TradeBuilder().Build();
+        _errorID = 0;
+        _errorDescription = "no error";
+    }
+
+    public OrderInfoBuilder WithMsg(string newMsg)
+    {
+        this._msg = newMsg;
+        return this;
+    }
+
+    public OrderInfoBuilder WithTrade(Trade newTrade)
+    {
+        this._trade = newTrade;
+        return this;
+    }
+
+    public OrderInfoBuilder WithErrorID(int newErrorID)
+    {
+        this._errorID = newErrorID;
+        return this;
+    }
+
+    public OrderInfoBuilder WithErrorDescription(string newErrorDescription)
+    {
+        this._errorDescription = newErrorDescription;
+        return this;
+    }
+
+    public OrderInfo Build()
+    {
+        return new OrderInfo()
+        {
+            Msg = _msg,
+            Trade = _trade,
+            ErrorID = _errorID,
+            ErrorDescription = _errorDescription,
+        };
+    }
+}
