a1596b798c563d9131253b9b6bbb84a299065323
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-09-08 15:05:12 -0700

    Action: Added OrderHistoryBuilder class.

---
+++ MetatraderSharp.Tests/Builders/MT4/History/OrderHistoryBuilder.cs
@@ -0,0 +1,54 @@
+﻿using MetatraderSharp.MTsocketAPI.Responses.MT4;
+
+namespace MetatraderSharp.Tests.Builders.MT4;
+
+public class OrderHistoryBuilder
+{
+    private string _msg;
+    private List<Trade> _trades;
+    private int _errorID;
+    private string _errorDescription;
+
+    public OrderHistoryBuilder()
+    {
+        _msg = "TRADE_HISTORY";
+        _trades = new TradeListBuilder().Build();
+        _errorID = 0;
+        _errorDescription = "no error";
+    }
+
+    public OrderHistoryBuilder WithMsg(string newMsg)
+    {
+        this._msg = newMsg;
+        return this;
+    }
+
+    public OrderHistoryBuilder WithTrades(List<Trade> newTrades)
+    {
+        this._trades = newTrades;
+        return this;
+    }
+
+    public OrderHistoryBuilder WithErrorID(int newErrorID)
+    {
+        this._errorID = newErrorID;
+        return this;
+    }
+
+    public OrderHistoryBuilder WithErrorDescription(string newErrorDescription)
+    {
+        this._errorDescription = newErrorDescription;
+        return this;
+    }
+
+    public OrderHistory Build()
+    {
+        return new OrderHistory()
+        {
+            Msg = _msg,
+            Trades = _trades,
+            ErrorID = _errorID,
+            ErrorDescription = _errorDescription,
+        };
+    }
+}
--- MetatraderSharp.Tests/Builders/MT4/Orders/Definitions/TradeBuilder.cs
+++ MetatraderSharp.Tests/Builders/MT4/Orders/Definitions/TradeBuilder.cs
@@ -159,5 +159,4 @@ public class TradeBuilder
             Expiration = _expiration,
         };
     }
-
 }
