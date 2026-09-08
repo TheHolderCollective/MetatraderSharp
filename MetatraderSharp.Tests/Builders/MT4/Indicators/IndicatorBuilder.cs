46a055595134e2e0478d605938d8b91387f271ff
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-09-08 12:48:53 -0700

    Action: Added IndicatorBuilder class.

---
+++ MetatraderSharp.Tests/Builders/MT4/Indicators/IndicatorBuilder.cs
@@ -0,0 +1,55 @@
+﻿using MetatraderSharp.MTsocketAPI.Responses.MT4;
+
+namespace MetatraderSharp.Tests.Builders.MT4;
+
+public class IndicatorBuilder
+{
+    private string _msg;
+    private double _dataValue;
+    private int _errorID;
+    private string _errorDescription;
+
+    public IndicatorBuilder()
+    {
+        _msg = "ATR_INDICATOR";
+        _dataValue = 7.714E-05;
+        _errorID = 0;
+        _errorDescription = "no error";
+    }
+
+    public IndicatorBuilder WithMsg(string newMsg)
+    {
+        this._msg = newMsg;
+        return this;
+    }
+
+    public IndicatorBuilder WithDataValue(double newDataValue)
+    {
+        this._dataValue = newDataValue;
+        return this;
+    }
+
+    public IndicatorBuilder WithErrorID(int newErrorID)
+    {
+        this._errorID = newErrorID;
+        return this;
+    }
+
+    public IndicatorBuilder WithErrorDescription(string newErrorDescription)
+    {
+        this._errorDescription = newErrorDescription;
+        return this;
+    }
+
+    public Indicator Build()
+    {
+        return new Indicator()
+        {
+            Msg = _msg,
+            DataValue = _dataValue,
+            ErrorID = _errorID,
+            ErrorDescription = _errorDescription,
+        };
+    }
+
+}
--- MetatraderSharp/MTsocketAPI.Responses/MT4/Indicators/Indicator.cs
+++ MetatraderSharp/MTsocketAPI.Responses/MT4/Indicators/Indicator.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+
 namespace MetatraderSharp.MTsocketAPI.Responses.MT4;
 
 public class Indicator
